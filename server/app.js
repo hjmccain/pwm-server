8c360f9d5019093cf12206ab6d6af2aa0409d501
hjmccain <hannahjmccain@gmail.com>
2017-09-12 14:13:35 -0400

    Re-push to Heroku.

--- server/app.js
+++ server/app.js
@@ -6,13 +6,8 @@ const moment = require('moment');
 const app = express();
 const con = require('./connection');
 
-console.log(process.env.CLEARDB_DATABASE_URL);
-
-// Serve static files from the React app.
-// Note: the path that you provide to the express.static function is relative to the directory from
-// where you launch your node process. If you run the express app from another directory, it’s safer to
-// use the absolute path of the directory that you want to serve.
-// app.use(express.static(path.join(__dirname, '../client/build')));
+console.log('Node app running');
+// console.log(process.env.CLEARDB_DATABASE_URL);
 
 app.use(bodyParser.json());
 
@@ -24,7 +19,7 @@ app.use((req, res, next) => {
 
 // COMPONENT TWO: EXPRESS MIDDLEWARE
 app.post('/feedback', (req, res) => {
-  console.log('POST request received.');
+  console.log(`POST request received at ${Date.now()}`);
   const userData = Object.assign({}, req.body, { date: moment().format() });
 
   const postToDB = new Promise((resolve, reject) => {
@@ -38,13 +33,5 @@ app.post('/feedback', (req, res) => {
       res.status(500).send('Warning! The data was not saved.');
     })
 });
-
-// catchall handler — any requests that don't match the one(s) outlined above
-// will be sent here.
-
-// app.get('*', (req, res) => {
-//   console.log('catchall endpoint');
-//   res.sendFile(path.join(__dirname + '../client/build/index.html'))
-// });
 
 module.exports = app;
